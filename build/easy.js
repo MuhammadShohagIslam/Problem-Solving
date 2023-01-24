f4353ebf214d4eecdec93bbb9b2e9eef793ef8b8
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2023-01-24 14:45:24 +0300

    Longest Substring Without Repeating Characters

---
+++ build/easy.js
@@ -0,0 +1,24 @@
+"use strict";
+/*
+    Longest Substring Without Repeating Characters
+*/
+function lengthOfLongestSubstring(s) {
+    if (s.length < 0) {
+        return s.length;
+    }
+    const seen = new Map();
+    let left = 0;
+    let longest = 0;
+    for (let right = 0; right < s.length; right++) {
+        const currentChar = s[right];
+        const previousSeenChar = seen.get(currentChar);
+        if (previousSeenChar >= left) {
+            left = previousSeenChar + 1;
+        }
+        seen.set(currentChar, right);
+        longest = Math.max(longest, right - left + 1);
+    }
+    return longest;
+}
+;
+console.log(lengthOfLongestSubstring("abcabcbb"));
